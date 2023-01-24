138a752616d5d489c099b1cb847ecab1f6753238
Julliver <giuliacassano1993@gmail.com>
2023-01-24 16:18:54 +0100

    birthdays

---
+++ birthday-reminder/src/Data.jsx
@@ -0,0 +1,43 @@
+export default [
+    {
+        id: 1,
+        name: 'Giulia Cassano',
+        age: 29,
+        image: 'https://media.meer.com/attachments/6cb905b85ad311d6acc81e537cbda6e553e0d299/store/fill/860/645/a10d4610745a1ed7ab0d925e1d9f682ab7e6486c96f7493b7e978bdf9568/Linfanticidio-nelle-scimmie-non-e-una-rarita.jpg',
+    },
+
+    {
+        id: 2,
+        name: 'Zara Dhaouadi',
+        age: 24,
+        image: 'https://www.momarte.com/blog/wp-content/uploads/come-disegnare-scimmia-primo-piano-1024x678.jpg',
+    },
+
+    {
+        id: 3,
+        name: 'Alessandro Armari',
+        age: 32,
+        image: 'https://www.my-personaltrainer.it/imgs/2021/07/22/vaiolo-delle-scimmie-orig.jpeg',
+    },
+
+    {
+        id: 4,
+        name: 'Nik Tizzani',
+        age: 34,
+        image: 'https://vigilidelfuoco.usb.it/fileadmin/_processed_/b/e/csm_scimmia_01_ed16ad0e75.jpg',
+    },
+
+    {
+        id: 5,
+        name: 'Davide Fella',
+        age: 35,
+        image: 'https://www.donnad.it/sites/default/files/styles/r_visual_d/public/201532/oroscopo-cinese-scimmia.jpg?itok=1t6s4s1k',
+    },
+
+    {
+        id: 4,
+        name: 'Marianna Laudisi',
+        age: 32,
+        image: 'https://www.donnaglamour.it/wp-content/uploads/2021/09/SH_scimmia_che_beve_acqua_dalla_bottiglia-640x350.jpg.webp',
+    },
+];
